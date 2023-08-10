7cc7cf513b3c0ccaff93e31acc76a5c31b5d6c62
Руслан <Stariy123@mail.ru>
2023-08-10 23:39:12 +0300

    v2.0

--- src/ResultBoard.java
+++ src/ResultBoard.java
@@ -1,47 +1,65 @@
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class ResultBoard {
-    TreeSet<Record> students = new TreeSet<>();
-    public ResultBoard() {
+    TreeMap <String, Float> boards = new TreeMap<>();
+    String name;
+    Float score;
+
+    public static void main(String[]args){
+        ResultBoard rb = new ResultBoard();
+
+        rb.addStudent("Sasha", 4.2f);
+        rb.addStudent("Masha", 4.8f);
+        rb.addStudent("Dasha", 3.9f);
+        rb.addStudent("Ira", 2.4f);
+        rb.addStudent("Alla", 1.5f);
+        rb.addStudent("Danila", 5.0f);
+
+        rb.showAll();
+
+
+
     }
 
-    void addStudent(String name, Float score) {
-        Record record = new Record();
-        record.name = name;
-        record.score = score;
+//    public ResultBoard(String name, Float score) {
+//        this.name = name;
+//        this.score = score;
+//    }
 
-        students.add(record);
+    void addStudent(String name, Float score) {
+        Board board = new Board();
+        board.name = name;
+        board.score = score;
+
+        boards.put(name, score);
+    }
+
+    void showAll(){
+        for (String name: boards.keySet()) {
+            System.out.println(boards.get(name));
+        }
     }
     List<String> top3 () {
         return null;
 
     }
 
-    public static void main(String[] args) {
-        ResultBoard rb = new ResultBoard();
-        rb.addStudent("Sasha", 4.5f);
-        rb.addStudent("Dasha", 3.5f);
-        rb.addStudent("Misha", 2.8f);
-        rb.addStudent("Polina", 5.0f);
-        rb.addStudent("Ruslan", 3.3f);
-        rb.addStudent("Alisa", 4.1f);
-        rb.addStudent("Adel", 2.3f);
-        rb.addStudent("Mustafa", 4.9f);
-        rb.addStudent("Givi", 3.1f);
-        rb.addStudent("Danila", 4.6f);
-        rb.addStudent("Alla", 3.6f);
+}
+class Board implements Comparator<Board> {
+    String name;
+    Float score;
 
-
-
-
-
-
+    @Override
+    public int compare(Board o1, Board o2) {
+        if(o1.score == o2.score) {
+            return 0;
+        } if (o1.score > o2.score) {
+            return 1;
+        } else return -1;
     }
 
-    class Record {
-        String name;
-        float score;
+    @Override
+    public String toString() {
+        return name + "has average mark: " + score ;
     }
-
 }
